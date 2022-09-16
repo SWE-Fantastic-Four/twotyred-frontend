e1d1f3edfbbfae8927a007f707e758ec651b0d73
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-09-16 13:48:29 +0800

    Added Login Page Boilerplate

--- src/layout/MainHeader.jsx
+++ src/layout/MainHeader.jsx
@@ -4,7 +4,7 @@ import P from '../constants/paths'
 
 const MainHeader = () => {
   return (
-    <nav className="flex w-full justify-center shadow-lg h-[98px] sticky top-0 bg-white">
+    <nav className="flex w-full justify-center shadow-lg h-[98px] sticky top-0 bg-white z-50">
       <div className="flex flex-row-reverse items-center max-w-[var(--max-screen-width)] w-full text-gray font-[24px]">
         <NavLink to={P.PROFILE}>
           <div className="rounded-full border h-[50px] w-[50px] mr-[24px]" />
--- src/views/login/Login.jsx
+++ src/views/login/Login.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import MainLayout from '../../layout/MainLayout';
+import bike from "../../assets/bike.png";
+import LoginInput from '../../components/LoginInput';
 
 const Login = () => {
   return (
-    <MainLayout>Login</MainLayout>
+    <MainLayout>
+      <main className="w-[var(--max-screen-width)]">
+        <img src={bike} className="absolute -left-96 top-12"/>
+        <img src={bike} className="absolute bottom-0 -right-96"/>
+        <div className="m-auto w-[494px] bg-off-white p-[60px] shadow-md rounded-[58px] font-extralight text-[32px] mt-[105px] flex flex-col">
+          <p className="mb-[40px]" >Sign In</p>
+          <div className='flex flex-col gap-[20px]'>
+            <LoginInput placeholder="enter email address" />
+            <LoginInput placeholder="enter password" />
+          </div>
+        </div>
+      </main>
+    </MainLayout>
   )
 }
 export default Login;
