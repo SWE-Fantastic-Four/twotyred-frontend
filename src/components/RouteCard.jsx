5f27ce41c59f80f38bea3bcb940421dd273e0016
IvanLoke <102502850+IvanLoke@users.noreply.github.com>
2022-10-05 23:46:17 +0800

    add routecard

---
+++ src/components/RouteCard.jsx
@@ -0,0 +1,106 @@
+import React from "react"
+import { StarIcon, HeartIcon, ArrowRightIcon, ArrowDownIcon } from "@heroicons/react/24/outline"
+import { StarIcon as StarIconSolid } from "@heroicons/react/24/solid";
+import { HeartIcon as HeartIconSolid} from "@heroicons/react/24/solid";
+import Map from "./Map"
+import "./styles.css"
+
+
+
+// function changefill(){
+//     setcolour(function(oldValue){
+//         return !oldValue;
+//     })
+//     console.log(colour)
+// }
+
+// function changefill(){
+//     console.log("fuck");
+//     setcolour("notshag");
+// }
+
+
+export default function RouteCard(){
+    const [starColour, setStarColour] = React.useState("black")
+    const [starFilled, setStarFilled] = React.useState(StarIcon);
+    const star ={
+        'fillstate' : starFilled,
+        'colourstate':starColour
+    }
+    function changefill(){
+        console.log(StarIcon)
+        setStarColour(function(oldValue){
+            return(starColour == "black"?"yellow-300":"black")
+        });
+
+    }
+    function staroutline(){
+        setStarFilled(function(oldValue){
+            return(starFilled == StarIcon ? StarIconSolid:StarIcon) 
+        })
+    }
+
+    function clickstar(){
+        changefill();
+        staroutline();
+    }
+
+    const[heartColour, setHeartColour] = React.useState("black")
+    const[heartFilled, setHeartFilled] = React.useState(HeartIcon)
+    const heart = {
+        'fillstate' : heartFilled,
+        'colourstate' : heartColour
+    }
+    function changeHeartColour(){
+        setHeartColour(function(oldValue){
+            return(heartColour == "black" ? "red-600":"black")
+        })
+    }
+    function changeHeartFill(){
+        setHeartFilled(function(oldValue){
+            return(heartFilled == HeartIcon ? HeartIconSolid:HeartIcon)
+        })
+    }
+
+    function clickHeart(){
+        changeHeartColour();
+        changeHeartFill();
+    }
+
+
+    return(
+        <div className = "wholecard w-[337px] h-[328px] rounded-[5px] border-[2px] border-solid border-dark-gray shadow-lg hover:border-black hover:cursor-pointer" >
+            <div className="map h-[216px] overflow-x-hidden">
+                <Map />      
+            </div>
+            <div className="stats w-[337px] h-[112px] flex flex-col">
+                <div className="first pl-[15px] pr-[12px] flex justify-between">
+                    <h1 className="title pt-[12px] font-[Roboto] font-bold text-[20px] leading-[20px] text-black ">My first cycle</h1>
+                    <div className="icons flex justify-[right]">
+                        <button onClick={clickstar}>
+                            <star.fillstate className={`star stroke-[3] mt-[7px] h-[18px] text-${star.colourstate}`} />
+                        </button>
+                        <button onClick={clickHeart}>
+                           <heart.fillstate className={`heart stroke-[3] mt-[7px] h-[19px] pl-[4px] pr-[4px] text-${heart.colourstate}`} /> 
+                        </button>
+                        <h1 className="totalLikes flex justify-start mt-2 font-bold text-[20px]">5000</h1>      
+                    </div>                  
+                </div>
+                <div className="second flex pt-[5px] pl-[15px]">
+                   <p className="startlocation font-[Roboto] font-normal text-[10px] leading-[12px] text-[#6B6B6B]">Boon Lay Place Market and Fo...</p>
+                    <ArrowRightIcon className="arrow w-[11.26px] h-[12]" />
+                    <p className="endlocation font-[Roboto] font-normal text-[10px] leading-[12px] text-[#6B6B6B]">Taman Jurong Food Centre | 5KM</p>                 
+                </div>
+                <div className="third flex pl-[15px] pt-[8px]">
+                    <img className="profilepic w-[34px] h-[34px] left-[122px] top-[589px]" src = 'src\assets\ProfilePic.svg'/>
+                    <div className="userinfo pt-[3px] pl-[5px]">
+                        <h1 className="name font-[Roboto] font-bold text-[12px] leading-[14px] text-black">@janedoe</h1>                     
+                        <p className="date font-[Roboto] font-normal text-[10px] leading-[12px] text-[#6B6B6B]">11/9/2022</p>                       
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
+    )
+}
--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import MainLayout from '../../layout/MainLayout';
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-
+import RouteCard from '../../components/RouteCard';
 const Dashboard = () => {
 
   return (
@@ -10,6 +10,7 @@ const Dashboard = () => {
         <div className="mt-[47px] flex">
           <span className="font-bold text-[64px] mr-[10px]">Explore Routes</span>
           <MagnifyingGlassIcon className="w-[64px] rotate-90 stroke-2" />
+          <RouteCard />
         </div>
       </div>
     </MainLayout>
