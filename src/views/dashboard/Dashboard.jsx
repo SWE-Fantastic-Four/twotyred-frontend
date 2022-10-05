d1f220412a4c7ab5087725c80ccd6969073b6925
IvanLoke <102502850+IvanLoke@users.noreply.github.com>
2022-10-05 23:52:20 +0800

    Update Dashboard.jsx

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -8,8 +8,8 @@ const Dashboard = () => {
     <MainLayout>
       <div className="w-[var(--max-screen-width)] px-[106px]">
         <div className="mt-[47px] flex">
-          <span className="font-bold text-[64px] mr-[10px]">Explore Routes</span>
-          <MagnifyingGlassIcon className="w-[64px] rotate-90 stroke-2" />
+          <span className="font-bold md:text-[64px] sm:text-[52px] text-[32px] mr-[10px]">Explore Routes</span>
+          <MagnifyingGlassIcon className="md:w-[64px] sm:w-[52px] w-[32.45px] rotate-90 stroke-2" />
           <RouteCard />
         </div>
       </div>
