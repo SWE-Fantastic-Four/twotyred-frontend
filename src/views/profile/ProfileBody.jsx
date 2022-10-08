bfb5834f7f201aedbe77bab891ebe9aacde399c9
kaijun123 <kaijun123@gmail.com>
2022-10-08 20:48:18 +0800

    Added route count

--- src/views/profile/ProfileBody.jsx
+++ src/views/profile/ProfileBody.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
-import { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
 import RouteCard from '../../components/RouteCard';
 
 const ProfileBody = ({ className }) => {
+  const username = useSelector(state => state.auth.displayName);
   const [showFavourites, setShowFavourites] = useState(false);
+  const [routeCount, updateRouteCount] = useState(0);
+  const [favouriteCount, updateFavouriteCount] = useState(0);
+
+  useEffect(() => {
+    const obtainRoutes = async () => {
+      const response = await fetch(`https://swe-backend.chayhuixiang.repl.co/routes/user/${username}`);
+      const data = await response.json();
+      updateRouteCount(data.routeGeometryArray.length)
+    }
+    
+    if (username) {
+      obtainRoutes();
+    }
+  }, [username]);
+
+  useEffect(() => {
+    const obtainFavouriteRoutes = async () => {
+      const response = await fetch(`https://swe-backend.chayhuixiang.repl.co/routes/user/${username}?favourite=true`);
+      const data = await response.json();
+      updateFavouriteCount(data.routeGeometryArray.length)
+    }
+    
+    if (username) {
+      obtainFavouriteRoutes();
+    }
+  }, [username]);
 
   return (
     <div className={`w-screen flex flex-col items-center ${className} font-medium sm:text-[15px] text-[11.1px]`}>
@@ -11,13 +38,13 @@ const ProfileBody = ({ className }) => {
         <div className="w-1/2 flex justify-center items-center">
           <div className={`border-b-[3px] ${showFavourites ? "border-transparent" : ""} flex items-center cursor-pointer sm:leading-[18px] leading-[14px] sm:pb-[4px] pb-[3px]`} onClick={() => setShowFavourites(false)}>
             <div className="mr-[3px]">Past Routes</div>
-            <div className="sm:px-[2px] px-[1.5px] sm:h-[17px] h-[13px] bg-gray rounded-full mb-[2px] pt-[1px]">3</div>
+            <div className="sm:px-[2px] px-[1.5px] sm:h-[17px] h-[13px] bg-gray rounded-full mb-[2px] pt-[1px]">{routeCount}</div>
           </div>
         </div>
         <div className="w-1/2 flex justify-center items-center">
           <div className={`border-b-[3px] ${!showFavourites ? "border-transparent" : ""} flex items-center cursor-pointer sm:leading-[18px] leading-[14px] sm:pb-[4px] pb-[3px]`} onClick={() => setShowFavourites(true)}>
             <div className="mr-[3px]">Favourites</div>
-            <div className="sm:px-[2px] px-[1.5px] sm:h-[17px] h-[13px] bg-gray rounded-full mb-[2px] pt-[1px]">0</div>
+            <div className="sm:px-[2px] px-[1.5px] sm:h-[17px] h-[13px] bg-gray rounded-full mb-[2px] pt-[1px]">{favouriteCount}</div>
           </div>
         </div>
       </div>
