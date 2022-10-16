d9af092d326cf240e291b0af1f88e118b468dc1d
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-10-16 22:33:41 +0800

    Update constants.js

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,5 +1,5 @@
 export const urls = {
-  lucky_pythonanywhere: "http://sqweedy.pythonanywhere.com",
+  lucky_pythonanywhere: "https://sqweedy.pythonanywhere.com",
   lucky_replit: "https://autorouteplotter.changdaozheng.repl.co",
   lucky_replit2: "https://swe-lucky.chayhuixiang.repl.co",
   backend: "https://SWE-Backend.chayhuixiang.repl.co",
