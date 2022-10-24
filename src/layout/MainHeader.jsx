73b0b7d2dd26f63f2978af7a8e25f125f9be8e5e
Chay Hui Xiang <chayhuixiang@gmail.com>
2022-10-24 10:06:47 +0800

    Fixed Dashboard Dropdown Bug

--- src/layout/MainHeader.jsx
+++ src/layout/MainHeader.jsx
@@ -24,6 +24,8 @@ const MainHeader = () => {
   
   const [showMobileMenu, setShowMobileMenu] = useState(false);
   const [mobileMenuHeight, setMobileMenuHeight] = useState(0);
+  const [createRouteHover, setCreateRouteHover] = useState(false);
+  const [profileHover, setProfileHover] = useState(false);
 
   const profilePhoto = useProfilePhoto();
 
@@ -75,7 +77,7 @@ const MainHeader = () => {
       {/* Desktop Navbar */}
       <nav className="hidden sm:flex w-full justify-center shadow-lg h-[98px] sticky top-0 bg-white z-50">
         <div className="flex flex-row-reverse items-center max-w-[var(--max-screen-width)] w-full text-dark-gray text-[24px] font-medium">
-          <div className="hover:border-b-[3px] h-full mr-[24px] flex items-center border-dark-gray relative group">
+          {/* <div className="hover:border-b-[3px] h-full mr-[24px] flex items-center border-dark-gray relative group">
             <img className={`${pathname === P.PROFILE ? "border-2 border-black" : "border"} rounded-full h-[50px] w-[50px]`} src={profilePhoto} />
             <div className="absolute top-[81px] transition-all opacity-0 scale-90 group-hover:scale-100 group-hover:opacity-100 w-[218px] h-[171px] right-0 translate-x-[12px] flex flex-col bg-[url('assets/ProfileSpeech.svg')]">
               <div className="h-[64px] w-[202px] mt-[32px] mx-auto">
@@ -87,10 +89,41 @@ const MainHeader = () => {
                 <img src={ProfileSpeechBtmDark} className="absolute invisible peer-hover:visible top-[98px] -z-10" />
               </div>
             </div>
+          </div> */}
+          <div className="hover:border-b-[3px] h-full mr-[24px] flex items-center border-dark-gray relative" onMouseOver={() => setProfileHover(true)} onMouseLeave={() => setProfileHover(false)}>
+            <img className={`${pathname === P.PROFILE ? "border-2 border-black" : "border"} rounded-full h-[50px] w-[50px]`} src={profilePhoto} />
+            <Transition
+              show={profileHover}
+              className="absolute top-[81px] w-[218px] h-[171px] right-0 translate-x-[12px] flex flex-col bg-[url('assets/ProfileSpeech.svg')]"
+              enter="transition-all"
+              leave="transition-all"
+              enterFrom="opacity-0 scale-90"
+              leaveTo="opacity-0 scale-90"
+              enterTo="opacity-100 scale-100"
+              leaveFrom="opacity-100 scale-100"
+            >
+              <div className="h-[64px] w-[202px] mt-[32px] mx-auto">
+                <Link to={P.PROFILE} className="h-full w-full hover:text-black cursor-pointer flex items-center justify-center peer">Profile</Link>
+                <img src={ProfileSpeechTopDark} className="absolute invisible peer-hover:visible top-[6px] -z-10" />
+              </div>
+              <div className="h-[64px] w-[202px] mx-auto">
+                <Link onClick={logoutHandler} to={P.LOGIN} className="h-full w-full hover:text-black cursor-pointer flex items-center justify-center peer">Logout</Link>
+                <img src={ProfileSpeechBtmDark} className="absolute invisible peer-hover:visible top-[98px] -z-10" />
+              </div>
+            </Transition>
           </div>
-          <div className={`${pathname === P.CREATEROUTE ? "text-black" : ""} hover:border-b-[3px] h-full mr-[41px] flex items-center border-dark-gray relative group`}>
+          <div className={`${pathname === P.CREATEROUTE ? "text-black" : ""} hover:border-b-[3px] h-full mr-[41px] flex items-center border-dark-gray relative`} onMouseOver={() => setCreateRouteHover(true)} onMouseLeave={() => setCreateRouteHover(false)}>
             <p>Create New Route</p>
-            <div className="absolute top-[81px] transition-all opacity-0 scale-90 group-hover:scale-100 group-hover:opacity-100 w-[242px] h-[171px] left-1/2 -translate-x-1/2 flex flex-col bg-[url('assets/CreateSpeech.svg')] ">
+            <Transition
+              show={createRouteHover}
+              className="absolute top-[81px] w-[242px] h-[171px] left-1/2 -translate-x-1/2 flex flex-col bg-[url('assets/CreateSpeech.svg')]"
+              enter="transition-all"
+              enterFrom="opacity-0 scale-90"
+              enterTo="scale-100 opacity-100"
+              leave="transition-all"
+              leaveFrom="scale-100 opacity-100"
+              leaveTo="opacity-0 scale-90"
+            >
               <div className="h-[64px] w-[226px] mt-[32px] mx-auto" >
                 <NavLink to={P.CREATEROUTE + "?page=0&mode=default"} className="h-full w-full hover:text-black cursor-pointer flex items-center justify-center peer">Customise Route</NavLink>
                 <img src={CreateSpeechTopDark} className="absolute left-1/2 -translate-x-1/2 invisible peer-hover:visible top-[6px] -z-10" />
@@ -99,7 +132,7 @@ const MainHeader = () => {
                 <NavLink to={P.CREATEROUTE + "?page=0&mode=lucky"} className="h-full w-full flex items-center justify-center peer hover:text-black cursor-pointer">I'm Feeling Lucky</NavLink>
                 <img src={CreateSpeechBtmDark} className="absolute left-1/2 -translate-x-1/2 invisible peer-hover:visible top-[98px] -z-10" />
               </div>
-            </div>
+            </Transition>
           </div>
           <NavLink to={P.DASHBOARD} className={({ isActive }) => (isActive ? "text-black" : "hover:border-b-[3px]") + " h-full mr-[40px] flex items-center border-dark-gray" }>
             <p>Dashboard</p>
